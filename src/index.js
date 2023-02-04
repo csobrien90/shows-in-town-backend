3b156c6ec55deebda2d7caffea765c0c515fc680
Chad OBrien <obrien.music@gmail.com>
2023-02-04 13:29:04 -0500

    switch from saving locally to writing events.json to s3

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import { writeFileSync } from 'fs'
-import { resolve, join } from 'path'
+import { S3Client, PutObjectCommand} from "@aws-sdk/client-s3"
 import { getEvents } from './getEvents.js'
 
 async function saveEvents() {
 	try {
-		const events = await getEvents()
-		const eventsJSON = JSON.stringify(events)
-		
-		// define __dirname to be the current directory and write events.json
-		const __dirname = resolve()
-		writeFileSync(join(__dirname, 'data', 'events.json'), eventsJSON)
+		// Create an S3 client service object
+		const s3 = new S3Client({ region: 'us-east-2' })
+
+		// Create the parameters for writing a JSON object with public ACL in S3
+		const params = {
+			Bucket: 'shows-in-town',
+			Key: 'events.json',
+			ACL: 'public-read',
+			ContentType: 'application/json',
+			Body: JSON.stringify(await getEvents())
+		}
+
+		// Write the object to S3
+		const data = await s3.send(new PutObjectCommand(params))
+		console.log('Success', data)
 
 	} catch (e) {
 		console.error(e)
